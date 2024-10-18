001bc6c10671c94b52b8e20fda3142887fb1ff2a
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    lemonade: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -20,17 +20,15 @@ PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2115
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
-
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 14 UKQ1.230924.001 R.1695d68-52ce-52cd release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:14/UKQ1.230924.001/R.1695d68-52ce-52cd:user/release-keys
+    BuildDesc="OnePlus9-user 14 UKQ1.230924.001 R.1695d68-52ce-52cd release-keys" \
+    BuildFingerprint=OnePlus/OnePlus9/OnePlus9:14/UKQ1.230924.001/R.1695d68-52ce-52cd:user/release-keys \
+    DeviceName=OnePlus9 \
+    DeviceProduct=OnePlus9 \
+    SystemDevice=OnePlus9 \
+    SystemName=OnePlus9
 
 # Matrixx
 MATRIXX_BUILD_TYPE := Official
